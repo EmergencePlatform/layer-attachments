9397b2e81cd923bd89fdbe133996017dc52752db
Chris Alfano <chris@jarv.us>
2019-11-06 04:22:23 +0000

    feat: add initial implementation of model and requset handler

---
+++ php-classes/Emergence/Attachments/Attachment.php
@@ -0,0 +1,192 @@
+<?php
+
+namespace Emergence\Attachments;
+
+use RuntimeException;
+use UnexpectedValueException;
+use Imagick;
+
+use Media;
+
+use Emergence\Site\Storage;
+
+class Attachment
+    extends \VersionedRecord
+    implements \Emergence\Interfaces\Image
+{
+    public static $storageBucketId = 'attachments';
+
+    public static $collectionRoute = '/attachments';
+    public static $tableName = 'attachments';
+    public static $singularNoun = 'attachment';
+    public static $pluralNoun = 'attachments';
+
+    public static $fields = [
+        'ContextClass',
+        'ContextID' => 'uint',
+        'ContentHash' => [
+            'type' => 'string',
+            'length' => 40
+        ],
+        'MIMEType',
+        'Title' => [
+            'default' => null
+        ],
+        'Status' => [
+            'type' => 'enum',
+            'values' => ['normal', 'removed'],
+            'default' => 'normal'
+        ]
+    ];
+
+    // public static $indexes = [
+    //     'Context' => [
+    //         'fields' => ['ContextClass', 'ContextID']
+    //     ]
+    // ];
+
+    public static $validators = [
+        'Context' => [
+            'validator' => 'require-relationship'
+        ]
+    ];
+
+    public static $relationships = [
+        'Context' => [
+            'type' => 'context-parent'
+        ]
+    ];
+
+    public static function createFromFile($filePath, array $fields = [], $autoSave = false)
+    {
+        $Attachment = static::create($fields);
+
+        $Attachment->loadContent($filePath);
+
+        if ($autoSave) {
+            $Attachment->save();
+        }
+
+        return $Attachment;
+    }
+
+    public static function createFromMedia(Media $Media, array $fields = [], $autoSave = false)
+    {
+        // manually create new instance so that CreatorID/Created can be set manually
+        $Attachment = new static(
+            [
+                'Created' => date('Y-m-d H:i:s', $Media->Created),
+                'CreatorID' => $Media->CreatorID
+            ],
+            true,
+            true
+        );
+
+        $Attachment->Context = $Media->Context;
+        $Attachment->Title = $Media->Caption;
+        $Attachment->setFields($fields);
+
+        $Attachment->loadContent($Media->FilesystemPath);
+
+        if ($autoSave) {
+            $Attachment->save();
+        }
+
+        return $Attachment;
+    }
+
+    public function loadContent($filePath)
+    {
+        // only allow if no content loaded yet
+        if ($this->ContentHash) {
+            throw new RuntimeException('cannot load content into attachment that already has content hash');
+        }
+
+
+        // parse type with ImageMagick
+        $image = new Imagick($filePath);
+
+        if (!$image->valid()) {
+            throw new UnexpectedValueException('media file is not a handleable format');
+        }
+
+        $mimeType = $image->getImageMimeType();
+
+
+        // calculate content hash via git method
+        $contentHash = exec('git hash-object '.escapeshellarg($filePath));
+
+
+        // upload to storage bucket
+        $bucket = Storage::getFilesystem(static::$storageBucketId);
+        $storagePath = "uploads/{$contentHash}";
+
+        if (!$bucket->has($storagePath)) {
+            $stream = fopen($filePath, 'r');
+            $uploadSuccessful = $bucket->writeStream($storagePath, $stream, [
+                'ContentType' => $mimeType
+            ]);
+            fclose($stream);
+
+            if (!$uploadSuccessful) {
+                throw new RuntimeException('failed to upload attachment to storage');
+            }
+        }
+
+
+        // write to model
+        $this->ContentHash = $contentHash;
+        $this->MIMEType = $mimeType;
+    }
+
+    public function getImage(array $options = [])
+    {
+        if (!$this->ContentHash) {
+            return null;
+        }
+
+
+        // load from storage bucket
+        $bucket = Storage::getFilesystem(static::$storageBucketId);
+        $storagePath = "uploads/{$this->ContentHash}";
+
+        if (!$bucket->has($storagePath)) {
+            throw new RuntimeException('attachment content not available in bucket: '.$storagePath);
+        }
+
+        if (!$storageStream = $bucket->readStream($storagePath)) {
+            throw new RuntimeException('failed to read attachment content from bucket: '.$storagePath);
+        }
+
+        $image = new Imagick();
+        $image->readImageFile($storageStream);
+        fclose($storageStream);
+
+        if (!$image->valid()) {
+            throw new UnexpectedValueException('image could not be read from attachment content: '.$storagePath);
+        }
+
+
+        // apply any type-specific changes
+        switch ($this->MIMEType) {
+            case 'application/pdf':
+                $image->setResolution(300, 300);
+                break;
+        }
+
+
+        // return Imagick intance
+        return $image;
+    }
+
+    public function getImageUrl($maxWidth = null, $maxHeight = null, array $options = [])
+    {
+        if ($maxWidth || $maxHeight) {
+            $path = sprintf('image/%u/%u', $maxWidth ?: $maxHeight, $maxHeight ?: $maxWidth);
+        } else {
+            $path = 'image';
+        }
+
+        return $this->getUrl($path);
+    }
+}
---
+++ php-classes/Emergence/Attachments/RequestHandler.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace Emergence\Attachments;
+
+use Imagick;
+use ImagickPixel;
+use RuntimeException;
+
+use Emergence\Site\Storage;
+
+
+class RequestHandler extends \RecordsRequestHandler
+{
+    public static $imageStorageBucketId = 'attachment-images';
+    public static $imageCompressionQuality = 90;
+
+    public static $recordClass = Attachment::class;
+    public static $accountLevelAPI = 'ReadOnlyStaff';
+    public static $accountLevelBrowse = 'ReadOnlyStaff';
+    public static $accountLevelRead = 'ReadOnlyStaff';
+    public static $accountLevelComment = 'Staff';
+    public static $accountLevelWrite = 'Staff';
+    public static $browseLimitDefault = 20;
+    public static $browseOrder = ['ID' => 'DESC'];
+
+    public static function handleRecordRequest(\ActiveRecord $Attachment, $action = false)
+    {
+        switch ($action ?: $action = static::shiftPath()) {
+            case 'image':
+                return static::handleImageRequest($Attachment);
+            default:
+                return parent::handleRecordRequest($Attachment, $action);
+        }
+    }
+
+    public static function handleImageRequest(Attachment $Attachment)
+    {
+        // check if configured
+        if (!$Attachment->ContentHash) {
+            return static::throwNotFoundError('attachment has no content yet');
+        }
+
+
+        // parse request
+        $maxWidth = static::shiftPath() ?: null;
+        $maxHeight = static::shiftPath() ?: $maxWidth;
+
+
+
+        // construct construct storage path
+        if ($maxWidth || $maxHeight) {
+            $storagePath = sprintf('%u/%u/%s', $maxWidth, $maxHeight, $Attachment->ContentHash);
+        } else {
+            $storagePath = sprintf('full/%s', $Attachment->ContentHash);
+        }
+
+
+        // load from storage
+        $storageBucket = Storage::getFilesystem(static::$imageStorageBucketId);
+
+        if (
+            $storageBucket->has($storagePath)
+            && $storageStream = $storageBucket->readStream($storagePath)
+        ) {
+            $image = new Imagick();
+            $image->readImageFile($storageStream);
+
+            header('Content-Type: '.$image->getImageMimeType());
+            rewind($storageStream);
+            fpassthru($storageStream);
+            fclose($storageStream);
+            return;
+        }
+
+
+        // load and configure image
+        $image = $Attachment->getImage();
+        $image->setImageAlphaChannel(Imagick::ALPHACHANNEL_REMOVE); // TODO: see what this do to pngs
+        $image->mergeImageLayers(Imagick::LAYERMETHOD_FLATTEN); // TODO: see what this do to things
+
+
+        // normalize orientation
+        switch ($image->getImageOrientation()) {
+            case Imagick::ORIENTATION_TOPLEFT:
+                break;
+            case Imagick::ORIENTATION_TOPRIGHT:
+                $image->flopImage();
+                break;
+            case Imagick::ORIENTATION_BOTTOMRIGHT:
+                $image->rotateImage(new ImagickPixel, 180);
+                break;
+            case Imagick::ORIENTATION_BOTTOMLEFT:
+                $image->flopImage();
+                $image->rotateImage(new ImagickPixel, 180);
+                break;
+            case Imagick::ORIENTATION_LEFTTOP:
+                $image->flopImage();
+                $image->rotateImage(new ImagickPixel, -90);
+                break;
+            case Imagick::ORIENTATION_RIGHTTOP:
+                $image->rotateImage(new ImagickPixel, 90);
+                break;
+            case Imagick::ORIENTATION_RIGHTBOTTOM:
+                $image->flopImage();
+                $image->rotateImage(new ImagickPixel, 90);
+                break;
+            case Imagick::ORIENTATION_LEFTBOTTOM:
+                $image->rotateImage(new ImagickPixel, -90);
+                break;
+            default: // Invalid orientation
+                break;
+        }
+        $image->setImageOrientation(Imagick::ORIENTATION_TOPLEFT);
+
+
+        // scale image if needed
+        if ($maxWidth || $maxHeight) {
+            $srcWidth = $image->getImageWidth();
+            $srcHeight = $image->getImageHeight();
+
+            $widthRatio = ($srcWidth > $maxWidth) ? ($maxWidth / $srcWidth) : 1;
+            $heightRatio = ($srcHeight > $maxHeight) ? ($maxHeight / $srcHeight) : 1;
+
+            $ratio = min($widthRatio, $heightRatio);
+
+            if ($ratio < 1) {
+                $scaledWidth = round($srcWidth * $ratio);
+                $scaledHeight = round($srcHeight * $ratio);
+
+                $image->resizeImage($scaledWidth, $scaledHeight, Imagick::FILTER_LANCZOS, 1);
+            }
+        }
+
+
+        // strip metadata but preserve ICC metadata
+        $imageProfiles = $image->getImageProfiles('icc', true);
+        $image->stripImage();
+
+        if ($imageProfiles && !empty($imageProfiles['icc'])) {
+            $image->profileImage('icc', $imageProfiles['icc']);
+        }
+
+
+
+        // determine output based on type
+        switch (strtoupper($image->getImageFormat())) {
+            case 'PDF':
+            case 'EPS':
+            case 'PS':
+                $image->setImageFormat('PNG');
+                break;
+            case 'JPEG':
+                $image->setCompressionQuality(static::$imageCompressionQuality);
+                $image->setImageCompression(Imagick::COMPRESSION_JPEG);
+                break;
+            case 'GIF':
+                break;
+            default:
+                // default everything else to PNG
+                $image->setImageFormat('PNG');
+                break;
+        }
+
+
+        // write to storage
+        $imageStream = fopen('php://temp', 'w+');
+        $image->writeImageFile($imageStream);
+        if (!$storageBucket->writeStream($storagePath, $imageStream)) {
+            throw new RuntimeException('unable to save image to');
+        }
+
+
+        // write to output
+        header('Content-Type: '.$image->getImageMimeType());
+        rewind($imageStream);
+        fpassthru($imageStream);
+        fclose($imageStream);
+    }
+}
